0a739592dd4129fe814026092d2fa0280d481c83
Mirek Truminski <miroslaw.truminski@gmail.com>
2022-05-21 11:36:47 +0200

    add support for sass and react-bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import './App.scss';
+import Button from 'react-bootstrap/Button';
 
 function App() {
     return (
         <div>
             <h2>Azeno</h2>
+            <Button variant="primary">Button #1</Button>
         </div>
     );
 }
