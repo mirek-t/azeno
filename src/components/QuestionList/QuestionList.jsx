54b943d26a86aa39b7c2e570435c82cfc894a2c0
Mirek Truminski <miroslaw.truminski@gmail.com>
2022-05-22 16:14:24 +0200

    get question from backend api

--- src/components/QuestionList/QuestionList.jsx
+++ src/components/QuestionList/QuestionList.jsx
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import Question from '../Question/Question';
 
 function QuestionList() {
+    const [questions, setQuestions] = useState([]);
+    const params = useParams();
+    const [question, setQuestion] = useState(null);
+
+    useEffect(() => {
+        getDeck(params.id).then((data) => setQuestions(data));
+    }, []);
+
+    useEffect(() => {
+        if (questions.length) {
+            setQuestion(questions[0]);
+        }
+    }, [questions]);
+
+    async function getDeck(id) {
+        const url = 'http://localhost:3010';
+        const endpoint = 'decks';
+        const response = await fetch(`${url}/${endpoint}/${id}/cards`);
+
+        return await response.json();
+    }
+
     return (
         <div>
-            <Question />
+            {!question ? (
+                <h2>Loading</h2>
+            ) : (
+                <Question question={question.question} answer={question.answer} />
+            )}
         </div>
     );
 }
